3d837dee7f8caeb76dc310c80c7b5ca8df339177
Rodrigo <alu0101154473@ull.edu.es>
2024-03-26 10:42:09 +0000

    Conflicts on activity_main.xml resolved

--- app/src/main/java/es/ull/etsii/testastrolabos/MainActivity.java
+++ app/src/main/java/es/ull/etsii/testastrolabos/MainActivity.java
@@ -1,6 +1,21 @@
 package es.ull.etsii.testastrolabos;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int FILE_READING_REQUEST_CODE = 1001;
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates;
+    SwitchCompat sw_locations_updates, sw_gps;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
